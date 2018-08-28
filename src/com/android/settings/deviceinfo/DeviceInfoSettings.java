bf158aacdb3b35eb1193593b56973d03cf291fe2
Chris <chris@cypheros.co>
2018-08-28 09:58:18 -0400

    deviceinfo: Move imei information to Sim status
    This information is sensitive and should not be in the forefront of the
    about phone menu.
    
    - Also removed phone number preference as this is already in sim status
    - Removed legal information preference
    
    Change-Id: I5d6fa97cd59f0f2f394d95497950afff3018bf69
    Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

--- src/com/android/settings/deviceinfo/DeviceInfoSettings.java
+++ src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -124,17 +124,11 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
             Activity activity, Fragment fragment, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new EmergencyInfoPreferenceController(context));
-        controllers.add(new PhoneNumberPreferenceController(context));
         controllers.add(new SimStatusPreferenceController(context, fragment));
         controllers.add(new DeviceModelPreferenceController(context, fragment));
-        controllers.add(new ImeiInfoPreferenceController(context, fragment));
         controllers.add(new IpAddressPreferenceController(context, lifecycle));
         controllers.add(new WifiMacAddressPreferenceController(context, lifecycle));
         controllers.add(new BluetoothAddressPreferenceController(context, lifecycle));
-        //controllers.add(new RegulatoryInfoPreferenceController(context));
-        controllers.add(new SafetyInfoPreferenceController(context));
-        controllers.add(new ManualPreferenceController(context));
-        //controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
