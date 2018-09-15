f225bee8925e1ea420b608d3b9b944bcd6bf6c18
Dan Willemsen <dwillemsen@google.com>
2018-09-14 22:57:11 -0700

    Settings: Remove obsolete LOCAL_JACK_ENABLED
    Bug: 73108676
    Test: none
    Change-Id: I6fd694b0efa5624317538b791ccecb984fdd7c0d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- Android.mk
+++ Android.mk
@@ -61,12 +61,6 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay \
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     packages/apps/DU-Tweaks/res
 
-ifneq ($(INCREMENTAL_BUILDS),)
-    LOCAL_PROGUARD_ENABLED := disabled
-    LOCAL_JACK_ENABLED := incremental
-    LOCAL_JACK_FLAGS := --multi-dex native
-endif
-
 include frameworks/base/packages/SettingsLib/common.mk
 include frameworks/base/packages/SettingsLib/search/common.mk
 include vendor/du/support/common.mk
