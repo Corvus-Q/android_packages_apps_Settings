b72147c68710f97388058ca1af1237060d75254f
Alex Cruz <alex@dirtyunicorns.com>
2018-10-04 17:05:07 -0500

    Utilize our support vendor
    Change-Id: I972e87d0ae9824643c89d7e9ec1a7dd110660206

--- Android.mk
+++ Android.mk
@@ -69,6 +69,7 @@ endif
 
 include frameworks/base/packages/SettingsLib/common.mk
 include frameworks/base/packages/SettingsLib/search/common.mk
+include vendor/du/support/common.mk
 
 include $(BUILD_PACKAGE)
 
