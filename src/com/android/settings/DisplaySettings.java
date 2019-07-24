240d91b2d9e499195e8ed3a6837f202a94376ae1
Genkzsz11 <genkzsz11@gmail.com>
2019-07-24 20:20:37 +0700

    Add QS header styles [2/3] *base https://github.com/Havoc-OS/android_packages_apps_HavocSettings/commit/6ab79fd922a4907e3a508b24d6b43231c54cf08a

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -38,6 +38,7 @@ import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.QsTileStylesPreferenceController;
+import com.android.settings.display.QsHeaderStylePreferenceController;
 import com.android.settings.display.SwitchStylePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
@@ -146,6 +147,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new SwitchStylePreferenceController(context));
+        controllers.add(new QsHeaderStylePreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         controllers.add(new ThemeCategoryController(context));
