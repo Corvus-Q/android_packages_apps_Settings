5d4a3fab42c041a4c3f8aa860ee62bd5143479d3
Genkzsz11 <Genkzsz11@gmail.com>
2019-12-08 07:38:43 +0700

    Revert "Allow to hide arrow for back gesture [2/2]"
    This reverts commit 2f8f7842bed038adda019c782c10466623ecf53e.

--- src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
+++ src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -23,10 +23,8 @@ import android.content.Context;
 import android.content.om.IOverlayManager;
 import android.os.Bundle;
 import android.os.ServiceManager;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.SeekBar;
-import android.widget.Switch;
 
 import com.android.settings.R;
 import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
@@ -38,8 +36,6 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
     private static final String TAG = "GestureNavigationBackSensitivityDialog";
     private static final String KEY_BACK_SENSITIVITY = "back_sensitivity";
     private static final String KEY_BACK_HEIGHT = "back_height";
-
-    private boolean mArrowSwitchChecked;
 
     public static void show(SystemNavigationGestureSettings parent, int sensitivity, int height) {
         if (!parent.isAdded()) {
@@ -69,18 +65,9 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
         seekBarSensitivity.setProgress(getArguments().getInt(KEY_BACK_SENSITIVITY));
         final SeekBar seekBarHeight = view.findViewById(R.id.back_height_seekbar);
         seekBarHeight.setProgress(getArguments().getInt(KEY_BACK_HEIGHT));
-        final Switch arrowSwitch = view.findViewById(R.id.back_arrow_gesture_switch);
-        mArrowSwitchChecked = Settings.Secure.getInt(getActivity().getContentResolver(),
-                Settings.Secure.HIDE_BACK_ARROW_GESTURE, 0) == 1;
-        arrowSwitch.setChecked(mArrowSwitchChecked);
-        arrowSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mArrowSwitchChecked = arrowSwitch.isChecked() ? true : false;
-            }
-        });
         return new AlertDialog.Builder(getContext())
-                .setTitle(R.string.back_gesture_settings_dialog_title)
+                .setTitle(R.string.back_options_dialog_title)
+                .setMessage(R.string.back_sensitivity_dialog_message)
                 .setView(view)
                 .setPositiveButton(R.string.okay, (dialog, which) -> {
                     int sensitivity = seekBarSensitivity.getProgress();
@@ -90,8 +77,6 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                     SystemNavigationGestureSettings.setBackHeight(getActivity(), height);
                     SystemNavigationGestureSettings.setBackSensitivity(getActivity(),
                             getOverlayManager(), sensitivity);
-                    Settings.Secure.putInt(getActivity().getContentResolver(),
-                            Settings.Secure.HIDE_BACK_ARROW_GESTURE, mArrowSwitchChecked ? 1 : 0);
                 })
                 .create();
     }
