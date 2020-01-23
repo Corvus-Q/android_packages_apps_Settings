05dd07de0fb3bbcd5150fddecac47572be38f96e
Joey Huab <joey@evolution-x.org>
2020-01-23 12:58:39 +0000

    Settings: Disable dashboards conditions and suggestions by default [2/2]

--- src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
@@ -65,7 +65,7 @@ public class SuggestionFeatureProviderImpl implements SuggestionFeatureProvider 
     @Override
     public boolean isSuggestionComplete(Context context, @NonNull ComponentName component) {
         if (Settings.System.getInt(context.getContentResolver(),
-                Settings.System.ENABLE_SUGGESTIONS, 1) == 0) return true;
+                Settings.System.ENABLE_SUGGESTIONS, 0) == 0);
         final String className = component.getClassName();
         if (className.equals(WallpaperSuggestionActivity.class.getName())) {
             return WallpaperSuggestionActivity.isSuggestionComplete(context);
