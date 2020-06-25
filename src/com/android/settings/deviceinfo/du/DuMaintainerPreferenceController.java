75609160aa66f61b6175f217a842fb2a13e1cc99
Jackeagle <jackeagle102@gmail.com>
2020-06-25 10:52:51 +0200

    Settings: Switch to string overlay for maintainer string
    - Define following overlay in your device tree
        <string name="bliss_maintainer"></string>
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I65ad87ee20547320c97f1f0261d988307744b89f

--- src/com/android/settings/deviceinfo/du/DuMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/du/DuMaintainerPreferenceController.java
@@ -17,7 +17,7 @@
 package com.android.settings.deviceinfo.du;
 
 import android.content.Context;
-import android.os.SystemProperties;
+import android.content.res.Resources;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -28,7 +28,6 @@ import com.android.settings.core.BasePreferenceController;
 public class DuMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "DuMaintainerPreferenceController";
-    private static final String ROM_PROPERTY = "ro.corvus.maintainer";
 
     public DuMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -39,8 +38,7 @@ public class DuMaintainerPreferenceController extends BasePreferenceController {
     }
 
     public CharSequence getSummary() {
-        String rom = SystemProperties.get(ROM_PROPERTY,
-                this.mContext.getString(R.string.device_info_default));
-        return rom;
+        String maintainer = mContext.getResources().getString(R.string.corvus_maintainer);
+        return maintainer;
     }
 }
